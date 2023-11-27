a22cc7e33f8b3638fc0f10022c8017a80534087f
MikiPM <miki-592007@yandex.ru>
2023-11-27 19:59:03 +0500

    Modified

--- js/main.js
+++ js/main.js
@@ -14,42 +14,12 @@ const NAMES = [
   'Анастасия',
   'Евдокия',
   'Ирина',
-  'Дарина',
-  'Яков',
-  'Антон',
-  'Иван',
-  'Василий',
-  'Максим',
-  'Леонид',
-  'Арсений',
-  'Тихон',
-  'Тарас',
-  'Никита',
-  'Кирилл',
-  'Валерий',
-  'Михаил',
-  'Нестор',
-  'Виктор',
-  'Тимофей',
-  'Артём',
-  'Дмитрий',
-  'Назар',
 ];
 
-const SIMILAR_OBJ_PROM = 25;
+const PICTURES_COUNT = 25;
 
-const getRandomIntegerArray = (a, b) => {
-  const arrayRandom = [];
-
-  function f() {
-    const result = Math.floor(Math.random() * (b - a + 1) + a);
-
-    while (arrayRandom.includes(result) === false) {
-      arrayRandom.push(result);
-    }
-    return arrayRandom.at(-1);
-  }
-};
+const PICTURE = [];
+const COMMENTS = [];
 
 const getRandomInteger = (min, max) => {
   const lower = Math.ceil(Math.min(min, max));
@@ -61,11 +31,31 @@ const getRandomInteger = (min, max) => {
 const getRandArrayElement = (elements) =>
   elements[getRandomInteger(0, elements.length - 1)];
 
-const createObjProm = () => ({
-  id: getRandomInteger(1, 25),
-  url: `photos/${getRandomInteger(1, 25)}/jpg`,
+const getRandomIntegerArray = (a, b) => {
+  const arrayRandom = [];
+
+  function inner() {
+    let result = getRandomInteger(a, b);
+
+    while (arrayRandom.includes(result)) {
+      result = getRandomInteger(a, b);
+    }
+    arrayRandom.push(result);
+    return result;
+  }
+  return inner;
+};
+
+const uniqueGenerator = getRandomIntegerArray(1, 25);
+
+const createPicture = () => ({
+  id: uniqueGenerator(),
+  url: `photos/${uniqueGenerator()}/jpg`,
   description: 'Просто красиво!',
   likes: getRandomInteger(15, 200),
+});
+
+const createComments = () => ({
   comments: {
     id: getRandomInteger(1, 1000),
     avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
@@ -74,100 +64,17 @@ const createObjProm = () => ({
   },
 });
 
-const similarObjProm = Array.from({ length: SIMILAR_OBJ_PROM }, createObjProm);
+for (let i = 0; i < PICTURES_COUNT; i++) {
+  PICTURE.push(createPicture());
+}
 
-console.log(similarObjProm);
+console.log(PICTURE);
 
-// кексобукинг
+for (let i = 0; i < PICTURES_COUNT; i++) {
+  COMMENTS.push(createComments());
+}
 
-const arrTitle = [
-  'Время быть первым!',
-  'Лучше поздо чем никогда!',
-  'Быть войном жить вечно!',
-];
+PICTURE.push(COMMENTS);
 
-const arrType = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-
-const arrCheckingAndCheckout = ['12:00', '13:00', '14:00'];
-
-const arrFeatures = [
-  'wifi',
-  'dishwasher',
-  'parking',
-  'washer',
-  'elevator',
-  'conditioner',
-];
-
-const arrDescription = [
-  'Что то описать, но что',
-  'Придумай придумай',
-  'Думы думал',
-];
-
-const arrPhotos = [
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-];
-
-const GENERATOR_COUNT = 10;
-
-const featuresRandom = new Array(
-  arrFeatures[Math.floor(Math.random() * arrFeatures.length)]
-);
-
-const photoRandom = new Array(
-  arrPhotos[Math.floor(Math.random() * arrPhotos.length)]
-);
-
-const getRandomLoc = (min, max, how) => {
-  const random = min + Math.random() * (max - min);
-  const newRandom = random.toFixed(how);
-  return newRandom;
-};
-
-const avatarCan = getRandomInteger(1, 10);
-
-const getAvatar = () => {
-  if (avatarCan < 10) {
-    const gen = `img/avatars/user0${avatarCan}.png`;
-    return gen;
-  } else {
-    const newGen = `img/avatars/user${avatarCan}.png`;
-    return newGen;
-  }
-};
-
-const NEW_OBJ_KEKSTOBOK = () => ({
-  author: {
-    avatar: getAvatar(avatarCan),
-  },
-  offer: {
-    title: getRandArrayElement(arrTitle),
-    address: {
-      lat: getRandomLoc(35.65, 35.7, 5),
-      lng: getRandomLoc(139.7, 139.8, 5),
-    },
-    price: getRandomInteger(1, 30),
-    type: getRandArrayElement(arrType),
-    rooms: getRandomInteger(1, 15),
-    guests: getRandomInteger(1, 12),
-    checking: getRandArrayElement(arrCheckingAndCheckout),
-    checkout: getRandArrayElement(arrCheckingAndCheckout),
-    features: featuresRandom,
-    description: getRandArrayElement(arrDescription),
-    photos: photoRandom,
-  },
-  location: {
-    lat: getRandomLoc(35.65, 35.7, 5),
-    lng: getRandomLoc(139.7, 139.8, 5),
-  },
-});
-
-const generationNewArrKeksobok = Array.from(
-  { length: GENERATOR_COUNT },
-  NEW_OBJ_KEKSTOBOK
-);
-
-console.log(generationNewArrKeksobok);
+console.log(COMMENTS);
+console.log(PICTURE);
